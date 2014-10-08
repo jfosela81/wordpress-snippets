c3074a0c1c6912e0831be71a8eb81c24f8f39e58
Jorge <jfosela81@gmail.com>
2014-10-08 12:50:50 +0200

    Create js-snippet.js
    La parte JS para lanzar el media uploader

---
+++ media-uploader/js-snippet.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function() {
+
+    current_id = '';
+
+// Fijamos el media uploader al elemento que deseemos
+    jQuery('.upload_image').click(function() {
+        // Capturamos su ID para usarlo después
+        current_id = jQuery(this).attr('id');
+        tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
+        return false;
+    });
+
+    window.send_to_editor = function(html) {
+        imgurl = jQuery('img',html).attr('src');
+        // El ID capturado antes, lo usamos para encontrar el input text "asociado". En mi caso, le puse el mismo
+        // ID pero añadiendo al final '_url'
+        jQuery('#'+current_id+'_url').val(imgurl);
+        tb_remove();
+    }
+
+});
